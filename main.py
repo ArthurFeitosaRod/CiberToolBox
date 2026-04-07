0f2ab5944c901d3b192a1c5713649f6cd582100e
Arthur Feitosa Rodrigues dos Santos <arthurfeitosar64@gmail.com>
2026-04-06 22:31:33 -0300

    Mudanças finais

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import customtkinter as ctk
 from LoginWindow import *
+
+class Inicializadora:
+    def __init__(self, janela_para_fechar):
+        #fechamento da janela de login de vez por todas
+        janela_para_fechar.destroy()
+        #testando se há comunicação
+        print("hello world")
+        #principal
+        self.app = MainWindows()
+        self.app.mainloop()
 class MainWindows(ctk.CTk):
     def __init__(self):
         super().__init__()
-        print("hello world")
-        self.geometry("500x500")
-        self.title("LOGIN")
-    def Desenhar(self):
-        pass
+        self.title("Sistema Principal")
+        self.geometry("400x300")
+
 if __name__ == "__main__":
     win = Janela_Login()
     win.mainloop()
