bc73fb660e18f9b0f3590563ac58d337c282438c
Arthur Feitosa Rodrigues dos Santos <arthurfeitosar64@gmail.com>
2026-06-18 23:10:32 -0300

    UPDATE ARQUITETURA DE SOFTWARE - Padrão MVC
    `Adicionado Funcionalidade de Autenticação em padrão MVC
    Programação Orientada ao Objeto Aprofundada, foi feito sistema de Organização de codigo, como forma de LOG
    Ajustes extras na logica do codigo.`

--- CiberToolBox v2/src/Views/login_view.py
+++ CiberToolBox v2/src/Views/login_view.py
@@ -1,4 +1,5 @@
 import customtkinter as ctk
+from src.Controller.auth_controller import Auth_validacao
 # --- CONFIGURAÇÕES DE TEMA ---
 ORANGE_FLIPPER = "#FF8C00"
 DARK_BG = "#222121"
@@ -11,7 +12,8 @@ class LoginClientView(ctk.CTk):
         self.geometry("600x350")
         self.resizable(False,False)
         self.configure(fg_color=DARK_BG)
-        
+        self.login_inicial()
+    def login_inicial(self):
     # Container principal para troca de telas
         self.lcd = ctk.CTkFrame(self, fg_color=LCD_COLOR, width=300, height=120, corner_radius=10)
         self.lcd.pack(pady=20)
@@ -22,11 +24,10 @@ class LoginClientView(ctk.CTk):
         self.lcd_label.place(relx=0.5, rely=0.5, anchor="center")
         self.user = ctk.CTkEntry(self, placeholder_text="Operator ID", width=250, border_color=ORANGE_FLIPPER)
         self.user.pack(pady=10)
-
         self.pwd = ctk.CTkEntry(self, placeholder_text="Access Code", show="*", width=250, border_color=ORANGE_FLIPPER)
         self.pwd.pack(pady=10)
 
         self.btn = ctk.CTkButton(
             self, text="UNLOCK SYSTEM", fg_color=ORANGE_FLIPPER, 
-            text_color="black", font=("Arial", 14, "bold"))
+            text_color="black", font=("Arial", 14, "bold"), command=lambda: Auth_validacao.validar_login(self.user, self.pwd))
         self.btn.pack(pady=20)
